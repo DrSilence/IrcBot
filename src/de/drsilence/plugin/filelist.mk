671dd4db1d3f6f52fe28d983c1a05c9a6330737b
DrSilence <DrSilence@users.noreply.github.com>
2015-07-16 02:29:20 +0200

    fixed path insides

--- src/de/drsilence/plugin/filelist.mk
+++ src/de/drsilence/plugin/filelist.mk
@@ -4,8 +4,8 @@
 
 #include $(PRJ)/src/de/drsilence/irclib/plugin/../filelist.mk
 
-filelist += $(PRJ)/src/de/drsilence/irclib/plugin/ApplicationAPI.java
-filelist += $(PRJ)/src/de/drsilence/irclib/plugin/Pluggable.java
-filelist += $(PRJ)/src/de/drsilence/irclib/plugin/PluginLoader.java
-filelist += $(PRJ)/src/de/drsilence/irclib/plugin/PluginManager.java
+filelist += $(PRJ)/src/de/drsilence/plugin/ApplicationAPI.java
+filelist += $(PRJ)/src/de/drsilence/plugin/Pluggable.java
+filelist += $(PRJ)/src/de/drsilence/plugin/PluginLoader.java
+filelist += $(PRJ)/src/de/drsilence/plugin/PluginManager.java
 
