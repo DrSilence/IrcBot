98e25d1d87ed08e0fc260d2593b9b8ab39557f8a
DrSilence <DrSilence@users.noreply.github.com>
2015-07-16 02:13:08 +0200

    Added Makefiles in plugin/ and updated Makefiles in drsilence/

--- src/de/drsilence/filelist.mk
+++ src/de/drsilence/filelist.mk
@@ -4,4 +4,5 @@
 
 include $(PRJ)/src/de/drsilence/irclib/filelist.mk
 include $(PRJ)/src/de/drsilence/ircbot/filelist.mk
+include $(PRJ)/src/de/drsilence/plugin/filelist.mk
 
---
+++ src/de/drsilence/plugin/filelist.mk
@@ -0,0 +1,11 @@
+##########################################
+# filelist.mk - unique for each directory
+#
+
+#include $(PRJ)/src/de/drsilence/irclib/plugin/../filelist.mk
+
+filelist += $(PRJ)/src/de/drsilence/irclib/plugin/ApplicationAPI.java
+filelist += $(PRJ)/src/de/drsilence/irclib/plugin/Pluggable.java
+filelist += $(PRJ)/src/de/drsilence/irclib/plugin/PluginLoader.java
+filelist += $(PRJ)/src/de/drsilence/irclib/plugin/PluginManager.java
+
