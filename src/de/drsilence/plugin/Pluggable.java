6c3e43f649e9a35fdfb37d0ced1ea8f0d18e4e94
DrSilence <DrSilence@users.noreply.github.com>
2015-07-17 02:06:40 +0200

    Added Author and Stuff

--- src/de/drsilence/plugin/Pluggable.java
+++ src/de/drsilence/plugin/Pluggable.java
@@ -5,5 +5,11 @@ public interface Pluggable {
 	public boolean start();
 	public boolean stop();
 	public void setPluginAPI(ApplicationAPI api);
+	
+	//Experimental:
+	public String getPluginName();
+	public String getPluginVersion();
+	public String getPluginAuthor();
+	public String getPluginDescription();
 
 }
