8a2765dc048856b7edc6640ad2c25865f47cd7b7
DrSilence <DrSilence@users.noreply.github.com>
2015-07-20 11:44:46 +0200

    Added UserList for testing.

--- src/de/drsilence/ircbot/Main.java
+++ src/de/drsilence/ircbot/Main.java
@@ -3,11 +3,15 @@
  */
 package de.drsilence.ircbot;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 
+import de.drsilence.ircbot.eventhandlers.UserLists;
 import de.drsilence.irclib.IrcActionEvent;
 import de.drsilence.irclib.IrcConnection;
 import de.drsilence.irclib.IrcMessages;
+import de.drsilence.irclib.IrcReplies;
 
 
 /**
@@ -15,13 +19,6 @@ import de.drsilence.irclib.IrcMessages;
  *
  */
 public class Main {
-
-	/**
-	 * 
-	 */
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
 
 	/**
 	 * @param args Start params.
@@ -32,34 +29,55 @@ public class Main {
 		final String pass   	= "oauth:7appgukzxulffvv59u93vbawcuncm5";
 		final String nick 		= "da_checker";
 		final String login 		= "da_checker";
-        final String channel 	= "#sniddyus";
+        final String channel 	= "#da_checker";
 
 		IrcConnection irc = new IrcConnection();
 		//test:
+		irc.addIrcActionEvent(new UserLists());
 		irc.addIrcActionEvent(new IrcActionEvent() {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			@Override
 			public void eventMsg(IrcConnection ircio, IrcMessages msg, Matcher m) {
-				// TODO Auto-generated method stub
 				super.eventMsg(ircio, msg, m);
 				switch (msg) {
 					case MSG_PRIVMSG:
-						System.out.println(m.group("nick")+": "+m.group("text"));
+						String line = String.format( 	"[%s] %20s : %s",
+														dateFormat.format(new java.util.Date()),
+														m.group("nick"),
+														m.group("text")
+														);
+						System.out.println(line);
 						if(
-							m.group("nick").toLowerCase().equals("dr_silence") &&
-							m.group("text").toLowerCase().equals("!quit")
+							m.group("nick").toLowerCase().trim().equals("dr_silence") &&
+							m.group("text").toLowerCase().trim().equals("!quit")
 						) {
-							ircio.irc_cmd_msg(channel, "OK Master!");
-							ircio.irc_send_raw( "QUIT : Bye!\r\n" );
+							ircio.irc_cmd_msg(channel, "Ok Master! I'll take my leave...");
+							ircio.irc_cmd_quit("Bye Bye!");
 						}
 						break;
+						
+					case MSG_UNKNOWN:
+						System.err.println("Error: MSG_UNKOWN -> '"+m.group("rawmessage")+"'");
 
 					default:
 						break;
 				}
 			}
+
+			@Override
+			public void eventNumeric(IrcConnection ircio, IrcReplies rply,
+					Matcher m) {
+				super.eventNumeric(ircio, rply, m);
+				// Just display all responses:
+				System.out.println(m.group("rawmessage"));
+			}
+			
 		});
 		irc.connect(server, nick, login, pass);
 		irc.irc_cmd_join( channel );
+		irc.irc_cmd_join("#northernlion");
+		// Send Irc v3 req to enable join/part notifies:
+		irc.irc_send_raw("CAP REQ :twitch.tv/membership\r\n");
 		irc.run( ); // BLOCKING !
         
 	}
