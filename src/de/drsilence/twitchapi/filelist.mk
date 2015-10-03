2e15cad6c71cc49c534b154a2dcb5a55c420c0a1
DrSilence <DrSilence@users.noreply.github.com>
2015-10-03 13:03:44 -0400

    added make system for twitchapi.

---
+++ src/de/drsilence/twitchapi/filelist.mk
@@ -0,0 +1,10 @@
+##########################################
+# filelist.mk - unique for each directory
+#
+
+#include $(PRJ)/src/de/drsilence/twitchapi/filelist.mk
+
+
+filelist += $(PRJ)/src/de/drsilence/twitchapi/Json.java
+filelist += $(PRJ)/src/de/drsilence/twitchapi/TwitchAPI.java
+
